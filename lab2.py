5a533aa49516614102bba9c328bbda374db0d60a
herrylo <herryloyopai@163.com>
2020-03-11 13:47:46 +0800

    set

--- lab2.py
+++ lab2.py
@@ -90,3 +90,60 @@ a1 = compose1(square, add_one)
 mul_three = lambda x: x * 3 
 a2 = compose1(mul_three, a1)
 print(a2(5));
+
+def composite_identity(f, g):
+    """
+    Return a function with one parameter x that returns True if f(g(x)) is
+    equal to g(f(x)). You can assume the result of g(x) is a valid input for f
+    and vice versa.
+
+    >>> add_one = lambda x: x + 1        # adds one to x
+    >>> square = lambda x: x**2
+    >>> b1 = composite_identity(square, add_one)
+    >>> b1(0)                            # (0 + 1)^2 == 0^2 + 1
+    True
+    >>> b1(4)                            # (4 + 1)^2 != 4^2 + 1
+    False
+    """
+    return lambda x: f(g(x))== g(f(x))
+
+b1 = composite_identity(square, add_one)
+print(b1(4));
+
+# 问题6
+def count_cond(condition):
+    """Returns a function with one parameter N that counts all the numbers from
+    1 to N that satisfy the two-argument predicate function CONDITION.
+
+    >>> count_factors = count_cond(lambda n, i: n % i == 0)
+    >>> count_factors(2)   # 1, 2
+    2
+    >>> count_factors(4)   # 1, 2, 4
+    3
+    >>> count_factors(12)  # 1, 2, 3, 4, 6, 12
+    6
+
+    >>> is_prime = lambda n, i: count_factors(i) == 2
+    >>> count_primes = count_cond(is_prime)
+    >>> count_primes(2)    # 2
+    1
+    >>> count_primes(3)    # 2, 3
+    2
+    >>> count_primes(4)    # 2, 3
+    2
+    >>> count_primes(5)    # 2, 3, 5
+    3
+    >>> count_primes(20)   # 2, 3, 5, 7, 11, 13, 17, 19
+    8
+    """
+    def countDef(n):
+        i,count = 1,0
+        while i <= n:
+            if condition(n, i):
+                count += 1;s
+            i += 1;
+        return count;
+    return countDef;
+    
+count_factors = count_cond(lambda n, i: n % i == 0)
+print(count_factors(12))
